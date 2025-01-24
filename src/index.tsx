9f35b990d17382bbadc72b42c646fd417518d8e6
Nicholas Drinovsky <drinovskyne@co.monterey.ca.us>
2025-01-23 17:36:31 -0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-
+const firebaseConfig: FirebaseOptions = {
+  appId: process.env.REACT_APP_DEV_API_KEY,
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+  measurementId: process.env.REACT_APP_DEV_MEASUREMENT_ID
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -27,7 +30,8 @@ const root = ReactDOM.createRoot(
 // Initialize firebase instance
 if (process.env.NODE_ENV === 'production'  ){
   fetch('/__/firebase/init.json').then(async response => {
-    initializeApp(await response.json());
+    const app = initializeApp(await response.json());
+    const analytics = getAnalytics(app);
     root.render(
       <React.StrictMode>
         <App />
@@ -36,7 +40,8 @@ if (process.env.NODE_ENV === 'production'  ){
   })
 }
 else{
-  initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   root.render(
     <React.StrictMode>
       <App />
